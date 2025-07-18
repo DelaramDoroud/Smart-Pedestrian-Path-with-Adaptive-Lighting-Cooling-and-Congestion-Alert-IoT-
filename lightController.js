772a9278229ba0d1068050e3f17fb1dd32cc90df
unknown <delaramdoroud82@gmail.com>
2025-07-18 14:50:08 +0200

    light controll compleed

---
+++ lightController.js
@@ -0,0 +1,21 @@
+function handleLightMessage(data, mqttClient, topicOut) {
+  const light = data.light;
+  console.log(`Light = ${light}`);
+
+  let brightness;
+  if (light < 2804) {
+    brightness = 255;
+  } else if (light < 3277) {
+    brightness = 100;
+  } else {
+    brightness = 0;
+  }
+
+  const controlMsg = JSON.stringify({ lights: brightness });
+  mqttClient.publish(topicOut, controlMsg);
+  console.log(`Published to ${topicOut}: ${controlMsg}`);
+}
+
+module.exports = {
+  handleLightMessage
+};
---
+++ main.js
@@ -0,0 +1,30 @@
+const mqtt = require('mqtt');
+const { handleLightMessage } = require('./lightController');
+const broker = 'mqtts://broker.hivemq.com';
+const topic = 'smartpath/data';
+topicOutLight = 'smartpath/lights/control';
+
+const client = mqtt.connect(broker);
+// console.log("Connecting to MQTT broker");
+client.on('connect', () => {
+    console.log('Connected to MQTT broker');
+    client.subscribe(topic, (err) => {
+        if (err) {
+            console.error('Subscription error:', err);
+        } else {
+            console.log(`Subscribed to topic: ${topic}`);
+        }
+    });
+});
+
+client.on('message', (topic, message) => {
+    console.log(`Received message on topic ${topic}:`, message.toString());
+    try {
+        const data = JSON.parse(message.toString());
+        if ('light' in data) {
+            handleLightMessage(data, client, topicOutLight);
+        }
+    } catch (error) {
+        console.error('Error processing message:', error);
+    }
+});
