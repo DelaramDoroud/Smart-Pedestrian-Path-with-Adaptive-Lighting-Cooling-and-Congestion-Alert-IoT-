2a65ad25e9a01f4aefbd4da3899d87c684955eb4
Aida Atarzadeh <attarzadehosseiniaida@gmail.com>
2025-07-24 15:47:18 +0200

    adding crowd conditions into light controller

--- lightController.js
+++ lightController.js
@@ -1,12 +1,12 @@
 function handleLightMessage(data, mqttClient, topicOut) {
   const light = data.light;
+  const crowd = data.crowd?.value ?? 0;
   console.log(`Light = ${light}`);
-
   let brightness;
   if (light < 2804) {
-    brightness = 255;
+    brightness = crowd > 0 ? 255 : 100;
   } else if (light < 3277) {
-    brightness = 100;
+    brightness = crowd > 0 ? 255 : 100;
   } else {
     brightness = 0;
   }
